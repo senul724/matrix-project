1e4ed902b3e7cfceb30445915125cd39b80959bc
Senul <hardlove.mrplus@gmail.com>
2023-05-28 19:31:24 +0530

    senul commits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+  <div className="w-full flex flex-row gap-2 mt-20">
+    <div className="w-1/3 bg-red-500 h-20">One</div>
+    <div className="w-1/3 bg-green-500 h-20">Two</div>
+    <div className="w-1/3 bg-blue-500 h-20">Three</div>
+  </div>);
 };
 
 export default Home;
