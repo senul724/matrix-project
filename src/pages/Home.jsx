f436a865f236fe9b410d7d4d72e8661f352b5157
Senul <hardlove.mrplus@gmail.com>
2023-05-28 20:05:26 +0530

    changed the text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
   <div className="w-full flex flex-row gap-2 mt-20">
     <div className="w-1/3 bg-red-500 h-20">One</div>
     <div className="w-1/3 bg-green-500 h-20">Two</div>
-    <div className="w-1/3 bg-blue-500 h-20">Three</div>
+    <div className="w-1/3 bg-blue-500 h-20">Threee</div>
   </div>);
 };
 
