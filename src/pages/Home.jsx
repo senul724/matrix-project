77f20733256253b77e007c94c72e07b437459d71
kushani622 <hasinikabotheju@gmail.com>
2023-05-28 20:08:40 +0530

    comit by kushani

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ const Home = () => {
   return (
   <div className="w-full flex flex-row gap-2 mt-20">
     <div className="w-1/3 bg-red-500 h-20">One</div>
-    <div className="w-1/3 bg-green-500 h-20">Twoo</div>
-    <div className="w-1/3 bg-blue-500 h-20">Threee</div>
+    <div className="w-1/3 bg-green-500 h-20">Two</div>
+    <div className="w-1/3 bg-blue-500 h-20">Three</div>
   </div>);
 };
 
