4b9a27e33dae2074f15a225cfec11901c43ecd90
kushani622 <hasinikabotheju@gmail.com>
2023-06-01 12:35:39 +0530

    commit by kush

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 const Home = () => {
   return (
-    <div className="flex items-center justify-center w-full bg-red-300 p-10 gap-3">
-      <div className="w-1/3 p-10 bg-black">  
+    <><div className="flex items-center justify-center w-full bg-red-300 p-10 gap-3">
+      <div className="w-1/3 p-10 bg-black">
       </div>
-      <div className="w-1/3 p-10 bg-black">  
+      <div className="w-1/3 p-10 bg-black">
       </div>
-    </div>
+    </div><div>
+        <h1 className="text-3xl font-semibold underline text-center">
+          OUR GROUP
+        </h1>
+
+      </div></>
   )
 };
 
