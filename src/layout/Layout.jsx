01b4252cbcfbfce19421f7296905849268136d5d
kushani622 <hasinikabotheju@gmail.com>
2023-06-01 18:39:42 +0530

    commited kushi

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -21,6 +21,13 @@ const Layout = () => {
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
+        <label class="relative block">
+  <span class="sr-only">Search</span>
+  <span class="absolute inset-y-0 left-0 flex pl-2">
+    <svg class="h-6 w-6 fill-slate-300" viewBox="0 0 20 20"></svg>
+  </span>
+  <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-3 pl-9 pr-5 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+</label>
       </nav>
 
       <div className="min-h-screen">
