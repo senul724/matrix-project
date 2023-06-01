9c445573dc110e4722460d934377d28bb35382dc
kushani622 <hasinikabotheju@gmail.com>
2023-06-01 13:40:38 +0530

    commit by kush

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,14 @@ const Home = () => {
       <div className="w-1/3 p-10 bg-black">
       </div>
     </div><div>
-        <h1 className="text-3xl font-semibold underline text-center">
+        <h1 className="text-3xl font-semibold text-center">
           OUR GROUP
         </h1>
+        <h2 className="text-2xl text-center transition-2s">
+           This is our amazing group.</h2>
+           <button class=" bg-green-200 hover:bg-green-300 active:bg-green-400 focus:outline-1 focus:ring focus:ring-green-700 ...">
+  For More <br>Details</br>
+</button>
 
       </div></>
   )
