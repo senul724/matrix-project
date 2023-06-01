0ddb990a9859f8a415e35164dff600a1d97baf77
kushani622 <hasinikabotheju@gmail.com>
2023-06-01 13:49:35 +0530

    commit by kushani

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,19 @@
 const Contact = () => {
   return <h1>Contact</h1>;
+  <ul role="list">
+    <li class="group/item hover:bg-slate-100 ...">
+      <img src="{person.imageUrl}" alt="" />
+      <div>
+        <a href="{person.url}">{person.Wasana}</a>
+        <p>{person.title}</p>
+      </div>
+      <a class="group/edit invisible hover:bg-slate-200 group-hover/item:visible ..." href="tel:{person.phone}">
+        <span class="group-hover/edit:text-gray-700 ...">Call</span>
+        <svg class="group-hover/edit:translate-x-0.5 group-hover/edit:text-slate-500 ...">
+        </svg>
+      </a>
+    </li>
+</ul>
 };
 
 export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,12 @@
 const Home = () => {
   return (
-    <><div className="flex items-center justify-center w-full bg-red-300 p-10 gap-3">
-      <div className="w-1/3 p-10 bg-black">
-      </div>
-      <div className="w-1/3 p-10 bg-black">
-      </div>
+    <><div className="flex items-center justify-center w-full bg-blend-soft-light p-10 gap-3">
     </div><div>
         <h1 className="text-3xl font-semibold text-center">
           OUR GROUP
         </h1>
         <h2 className="text-2xl text-center transition-2s">
            This is our amazing group.</h2>
-           <button class=" bg-green-200 hover:bg-green-300 active:bg-green-400 focus:outline-1 focus:ring focus:ring-green-700 ...">
-  For More <br>Details</br>
-</button>
-
       </div></>
   )
 };
