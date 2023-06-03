ca6cf14096fcc6842bff39e146db3e778d86d393
kushani622 <hasinikabotheju@gmail.com>
2023-06-03 16:00:21 +0530

    commit by kush

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ const Home = () => {
   return (
     <div className="flex items-center justify-center w-full">
     <div className="flex items-center justify-center w-4/5 bg-blend-soft-light p-10 gap-3 ">
-      <div className="w-1/2 ml-32">
+      <div className="w-1/3 ml-32">
         <h1 className="text-3xl font-mono">
           OUR GROUP
         </h1>
         <h2 className="text-xl transition-2s font-mono">
            This is our amazing group.</h2>
       </div>
-      <div className="w-1/2 ">
+      <div className="w-2/3 ">
         <img src="/animated-grp.jpg" alt="" />
       </div>
     </div>
